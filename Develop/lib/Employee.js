0c046de8b45108daeca3cff49138a7b551cdc22d
Christina Starr <christinastarr19@gmail.com>
2020-06-30 17:36:01 -0400

    created getName(), getId(), getEmail() functions in Employee.js and they all passed.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,11 +1,22 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-}
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
 
+  getName(name) {
+    return this.name;
+  }
+
+  getId(id) {
+      return this.id;
+  }
+
+  getEmail() {
+      return this.email;
+  }
+}
 
 module.exports = Employee;
